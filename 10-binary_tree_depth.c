a5d2464dc62a2cdde8f1c06f40da05347fe53de6
Nahima-ayni <nahimamadey1@gmail.com>
2024-02-28 12:00:08 +0300

    width

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -11,7 +11,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
-	while (!tree)
+	while (tree->parent != NULL)
 	{
 		depth = depth + 1;
 		tree = tree->parent
