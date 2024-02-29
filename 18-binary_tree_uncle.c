34a97a9e4eba98988a7e702e065d988697cfa914
Nahima-ayni <nahimamadey1@gmail.com>
2024-02-29 17:07:07 +0300

    binary tree

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -16,10 +16,10 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-        if (node == NULL || node->parent == NULL)
-                return (NULL);
-        if (node == node->parent->left)
-                return (node->parent->right);
-        else
-                return (node->parent->left);
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node == node->parent->left)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
 }
